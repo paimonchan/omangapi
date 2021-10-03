7cb795518c1f94181f1368e6e0ba47178178d49a
Paimon <n.h.shounen@gmail.com>
2021-10-03 09:25:57 +0700

    add column color on table anima_tag

--- custom-addons/base_anima/models/anima_tag.py
+++ custom-addons/base_anima/models/anima_tag.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+from random import randint
 
 class AnimaTag(models.Model):
     _name = 'anima.tag'
 
+    def _get_default_color(self):
+        return randint(1, 11)
+
     name = fields.Char(required=True)
     is_genre = fields.Boolean(default=False)
+    color = fields.Integer(default=_get_default_color)
