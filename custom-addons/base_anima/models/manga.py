467758e74462f7f18907be7bd4c80ca84d16721d
Paimon <n.h.shounen@gmail.com>
2021-10-05 07:09:20 +0700

    refactor some naming fields

--- custom-addons/base_anima/models/manga.py
+++ custom-addons/base_anima/models/manga.py
@@ -10,16 +10,16 @@ class Manga(models.Model):
     alt_name = fields.Char()
     description = fields.Text()
     name = fields.Char(required=True)
-    source_id = fields.Char(required=True, index=True)
-    source = fields.Selection(const.MANGA_SOURCE_SELECTION)
+    source_id = fields.Char(string='Source Id', required=True, index=True)
+    source = fields.Selection(const.MANGA_SOURCE_SELECTION, string='Source')
     chapter_ids = fields.One2many('manga.chapter', 'manga_id')
     chapter_count = fields.Integer(compute='_compute_chapter_count', store=True)
     tag_ids = fields.Many2many('anima.tag')
-    tag_normalize = fields.Char(compute='_compute_tags')
-    genre_normalize = fields.Char(compute='_compute_tags')
+    tag_normalize = fields.Char(compute='_compute_tags', store=True)
+    genre_normalize = fields.Char(compute='_compute_tags', store=True)
     title_ids = fields.One2many(
         'anima.attribute', 'manga_id', domain=[('type', '=', const.ATTRIBUTE_TYPE_TITTLE)])
-    title_normalize = fields.Char(compute='_compute_titles')
+    title_normalize = fields.Char(compute='_compute_titles', store=True)
     description_ids = fields.One2many(
         'anima.attribute', 'manga_id', domain=[('type', '=', const.ATTRIBUTE_TYPE_DESCRIPTION)])
     content_rating = fields.Selection(const.CONTENT_RATING_SELECTION)
@@ -40,9 +40,9 @@ class Manga(models.Model):
     def _compute_tags(self):
         for record in self:
             tag_ids = record.tag_ids
-            genre_ids = tag_ids.filtered(lambda t: t.genre)
-            self.tags_normalize = ', '.join(tag_ids.mapped('name'))
-            self.genres_normalize = ', '.join(genre_ids.mapped('name'))
+            genre_ids = tag_ids.filtered(lambda t: t.is_genre)
+            self.tag_normalize = ', '.join(tag_ids.mapped('name'))
+            self.genre_normalize = ', '.join(genre_ids.mapped('name'))
 
     @api.depends('title_ids')
     def _compute_titles(self):
