886b2847255de3da39fc42149de97f9989e00325
Paimon <n.h.shounen@gmail.com>
2021-10-07 08:15:39 +0700

    add model for anima author

--- custom-addons/base_anima/models/__init__.py
+++ custom-addons/base_anima/models/__init__.py
@@ -6,3 +6,4 @@ from . import manga_page
 from . import anima_attribute
 from . import anima_tag
 from . import anima_social_media
+from . import anima_author
---
+++ custom-addons/base_anima/models/anima_author.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+from ..helpers import const
+
+class AnimaAuthor(models.Model):
+    _name = 'anima.author'
+    _description = 'Author'
+
+    name = fields.Char(required=True)
+    manga_ids = fields.Many2many(
+        'manga', 'manga_author_rel', 'author_id', 'manga_id')
+    social_ids = fields.One2many(
+        'anima.social.media', 'author_id')
