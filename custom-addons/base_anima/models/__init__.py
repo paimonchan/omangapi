18931566fd44e8031b7022f736c343111f442e81
Paimon <n.h.shounen@gmail.com>
2021-10-07 08:15:01 +0700

    add model for anima social media

--- custom-addons/base_anima/models/__init__.py
+++ custom-addons/base_anima/models/__init__.py
@@ -5,3 +5,4 @@ from . import manga_chapter
 from . import manga_page
 from . import anima_attribute
 from . import anima_tag
+from . import anima_social_media
---
+++ custom-addons/base_anima/models/anima_social_media.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+from ..helpers import const
+
+class AnimaSocialMedia(models.Model):
+    _name = 'anima.social.media'
+    _description = 'Social Media'
+    _rec_name = 'type'
+
+    author_id = fields.Many2one('anima.author')
+    type = fields.Selection(
+        const.SOCIAL_MEDIAL_TYPE_SELECTION, required=True)
+    url = fields.Char()
