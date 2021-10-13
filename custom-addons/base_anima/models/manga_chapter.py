fd3e9179cede96e7926a87869b05afdd8e271656
Paimon <n.h.shounen@gmail.com>
2021-10-13 08:38:00 +0700

    adjust column type data

--- custom-addons/base_anima/models/manga_chapter.py
+++ custom-addons/base_anima/models/manga_chapter.py
@@ -5,8 +5,10 @@ from odoo import models, fields, api
 class MangaChapter(models.Model):
     _name = 'manga.chapter'
     _description = 'Manga Chapter'
+    _rec_name = 'chapter'
 
-    chapter = fields.Char(required=True)
+    volume = fields.Integer(default=0)
+    chapter = fields.Integer(required=True)
     manga_id = fields.Many2one('manga', 'Manga', ondelete='cascade')
     page_ids = fields.Many2one('manga.page', 'Manga Page')
     source_id = fields.Char(required=True)
